73d629e8360eac57b3ad9bb6d0d8725dc8d08a46
Ilzar Abdrashitov <ilzarik811@gmail.com>
2025-08-04 15:25:25 +1000

    So far the not working calculator added and a dataset

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,5 +1,56 @@
 package src;
 
+import java.util.HashMap;
+
 public class Calculator {
-    
+
+    public static HashMap<String, Double> genreMap = new HashMap<>();
+
+    public Calculator(){
+        genreMap.put("Horror", (double) 0);
+        genreMap.put("Action", (double) 1);
+        genreMap.put("Adventure", (double) 2);
+        genreMap.put("Animation", (double) 3);
+        genreMap.put("Biography", (double) 4);
+        genreMap.put("Comedy", (double) 5);
+        genreMap.put("Crime", (double) 6);
+        genreMap.put("Drama", (double) 7);
+        genreMap.put("Fantasy", (double) 8);
+        genreMap.put("History", (double) 9);
+        genreMap.put("Musical", (double) 10);
+        genreMap.put("Mystery", (double) 11);
+        genreMap.put("Romance", (double) 12);
+        genreMap.put("SciFi", (double) 13);
+        genreMap.put("Thriller", (double) 14);
+        genreMap.put("War", (double) 15);
+        genreMap.put("Western", (double) 16);
+        genreMap.put("Documentary", (double) 17);
+        genreMap.put("Family", (double) 18);
+        genreMap.put("Sport", (double) 19);
+        genreMap.put("Short", (double) 20);
+        genreMap.put("Music", (double) 21);
+        genreMap.put("News", (double) 22);
+        genreMap.put("Reality", (double) 23);
+        genreMap.put("Talk", (double) 24);
+        genreMap.put("FilmNoir", (double) 25);
+        genreMap.put("GameShow", (double) 26);
+        genreMap.put("Adult", (double) 27);
+        genreMap.put("Experimental", (double) 28);
+        genreMap.put("Superhero", (double) 29);
+
+    }
+
+    /*Calculates the total points for the suggestion */
+    public static Double calculate(String genre, String rating, String budget){
+        Double totalPoints = Double.parseDouble(rating);
+        totalPoints += Double.parseDouble(budget.substring(2,budget.length()-1));
+        if(genreMap.get(genre.trim())==null){
+            totalPoints = 0.0;
+        }
+        else{
+            totalPoints += genreMap.get(genre);
+        }
+
+        return totalPoints;
+    }
 }
--- src/Reader.java
+++ src/Reader.java
@@ -3,34 +3,31 @@ package src;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.HashMap;
 import java.util.List;
 
 public class Reader {
 
-    public static String [][] moviesArray;
+    public static HashMap<String, Double> calcMap = new HashMap<>();
 
     /*Reads and splits words into a 2d array of lines and words */
     public static void read(String filePath) throws IOException{
 
         List<String> lines = Files.readAllLines(Paths.get(filePath));
-        moviesArray = new String [lines.size()][];
 
         for(int i = 0; i < lines.size(); i++){
 
             String line = lines.get(i).trim();
-            moviesArray[i] = line.split(",");
+
+            calcMap.put(line.split(",")[0], Calculator.calculate(line.split(",")[1],line.split(",")[2],line.split(",")[3]));
             
         }
     }
     
     /*Prints the moviesArray for debugging purposes */
     public static void printMoviesArray() {
-        for (int i = 0; i < moviesArray.length; i++) {
-            System.out.print("Line " + (i + 1) + ": ");
-            for (String word : moviesArray[i]) {
-                System.out.print(word + " ");
-            }
-            System.out.println();
+        for (String name : calcMap.keySet()){
+            System.out.println(name + " - " + calcMap.get(name));
         }
     }
 }
